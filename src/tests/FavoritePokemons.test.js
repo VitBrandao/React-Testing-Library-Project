f3caeeb1c74ac91cacdacd1af25468c729e58d3d
vitor <vitorsbsilva@gmail.com>
2021-11-30 16:36:45 -0300

    Commit extra para ver se o avaliador passa o Req.2

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../RenderWIthRouter';
+
+describe('Testa o componente Favorite Pokemons', () => {
+    it('Teste se é exibido na tela a mensagem No favorite pokemon found', () => {
+        
+    })
+})
