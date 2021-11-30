52094fb7d09ddeed9187f3c98972e271c37a45e6
vitor <vitorsbsilva@gmail.com>
2021-11-30 16:52:33 -0300

    Req.3 - feito e testado. Espero que o avaliador rode

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,11 +1,22 @@
-import { screen } from '@testing-library/react';
 import React from 'react';
-import userEvent from '@testing-library/user-event';
-import App from '../App';
+import { screen, render } from '@testing-library/react';
 import renderWithRouter from '../RenderWIthRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import pokemons from '../data';
 
 describe('Testa o componente Favorite Pokemons', () => {
-    it('Teste se é exibido na tela a mensagem No favorite pokemon found', () => {
-        
-    })
-})
+  it('Testa se é exibido na tela a mensagem No Favorite Pokemon Found', () => {
+    render(<FavoritePokemons pokemons={ [] } />);
+
+    const noFoundText = screen.getByText(/No Favorite Pokemon Found/i);
+    expect(noFoundText).toBeDefined();
+  });
+
+  it('Testa se é exibido todos os cards de pokémons favoritados', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ pokemons } />);
+
+    const pokemonsInTheScreen = screen.queryAllByText('pokemon-name');
+
+    pokemonsInTheScreen.map((pokemon) => expect(pokemon).toBeDefined());
+  });
+});
