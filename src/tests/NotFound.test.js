3c223b8d6d1a0d02172ae8f93eef63d8e0ce6454
vitor <vitorsbsilva@gmail.com>
2021-11-30 17:07:59 -0300

    Req.4 - feito

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Testando o componente NotFound', () => {
+  it('Testa se página contém um heading h2 com o devido texto', () => {
+    render(<NotFound />);
+
+    const notFoundText = screen.getByRole('heading',
+      { name: /Page requested not found/i });
+
+    expect(notFoundText).toBeDefined();
+  });
+
+  it('Testa se página mostra a imagem', () => {
+    render(<NotFound />);
+
+    const imageNotFound = screen.getByRole('img', { name: /not-found-image/i });
+    const URL = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(imageNotFound.src).toBe(URL);
+  });
+});
