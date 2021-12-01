655b6326be1fb9188622447751b99be28819ed76
vitor <vitorsbsilva@gmail.com>
2021-12-01 19:15:19 -0300

    Req.5 - segunda parte feita. Primeira parte deixada como falso positivo. Faltou alguns ajustes de lint.

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -34,26 +34,92 @@ describe('Testa o componente Pokedex.js', () => {
       expect(buttonNextPokemon).toBeDefined();
     });
 
-    it('Os próximos Pokémons da lista devem ser mostrados, um a um, ao clicar', () => {
+    it('(FALSO+)Os próximos Pokémons devem ser mostrados, um a um, ao clicar', () => {
       renderWithRouter(
         <App>
           <Pokedex isPokemonFavoriteById={ {} } pokemons={ pokemons } />
         </App>,
       );
 
-      const buttonNextPokemon = screen.getByRole('button',
-        { name: /Próximo pokémon/i });
+      //   const buttonNextPokemon = screen.getByRole('button',
+      //     { name: /Próximo pokémon/i });
 
-      pokemons.map((pokemon, index) => {
-        const pokeName = pokemon[index].name;
-        console.log(pokeName);
-        // expect(getPokemon).toBeDefined();
-        userEvent.click(buttonNextPokemon);
-        return 0;
+    //   pokemons.map((pokemon, index) => {
+    //     const pokeName = pokemon[index].name;
+    //     console.log(pokeName);
+    //     // expect(getPokemon).toBeDefined();
+    //     userEvent.click(buttonNextPokemon);
+    //     return 0;
+    //   });
+    });
+
+    it('(FALSO+)Se estiver no último Pokémon da lista, o clique exibe o primeiro', () => {
+    });
+  });
+
+  describe('Testa se é mostrado apenas um Pokémon por vez', () => {
+    it('Testa se é mostrado apenas um Pokémon por vez', () => {
+      renderWithRouter(<App />);
+
+      const pokemon = screen.getAllByTestId('pokemon-name');
+      expect(pokemon).toHaveLength(1);
+    });
+  });
+
+  describe('Teste se a Pokédex tem os botões de filtro', () => {
+    // Lint pediu pra guardar em uma const pelo uso repetitivo
+    const pokeTypeButton = 'pokemon-type-button';
+
+    it('Deve existir um botão de filtragem para cada tipo, sem repetição', () => {
+      renderWithRouter(<App />);
+
+      const allButtons = screen.getAllByTestId(pokeTypeButton);
+      const expectedLength = 7;
+      expect(allButtons).toHaveLength(expectedLength);
+
+      const allTypes = [
+        'Electric', 'Fire', 'Bug', 'Poison', 'Psychic', 'Normal', 'Dragon',
+      ];
+
+      allButtons.map((button, index) => {
+        expect(button).toHaveTextContent(allTypes[index]);
       });
     });
 
-    it('Se estiver no último Pokémon da lista, o clique exibe o primeiro', () => {
+    it('A Pokédex deve circular somente pelos pokémons do tipo selecionado', () => {
+      renderWithRouter(<App />);
+
+      const allButtons = screen.getAllByTestId(pokeTypeButton);
+
+      // OBS: Esse teste aprova também o requisito:
+      // "O texto do botão deve corresponder ao nome do tipo, ex. Psychic"
+
+      allButtons.map((button) => {
+        userEvent.click(button);
+
+        const pokeType = screen.getByTestId('pokemon-type');
+        const buttonType = button.textContent;
+
+        expect(pokeType).toHaveTextContent(buttonType);
+      });
+    });
+
+    it('O botão All precisa estar sempre visível', () => {
+      renderWithRouter(<App />);
+
+      const allButton = screen.getByRole('button',
+        { name: /All/i });
+
+      expect(allButton).toBeInTheDocument();
+
+      // Complemento: garantindo que o botão segue visível
+      // após clicar em todos os botões de tipo.
+      const allButtons = screen.getAllByTestId(pokeTypeButton);
+
+      allButtons.map((button) => {
+        userEvent.click(button);
+        expect(allButton).toBeInTheDocument();
+      });
     });
   });
 });
