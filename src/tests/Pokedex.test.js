585fe9d8042e24d2cba45d1fc7056c271be8fa96
vitor <vitorsbsilva@gmail.com>
2021-12-02 10:30:21 -0300

    Req.5 - finalizada primeira parte. Requisito finalizado. Lint e Stryker aprovados

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -8,17 +8,16 @@ import App from '../App';
 
 describe('Testa o componente Pokedex.js', () => {
   it('Teste se página contém um heading h2 com o texto Encountered pokémons', () => {
-    renderWithRouter(
-      <App>
-        <Pokedex isPokemonFavoriteById={ {} } pokemons={ pokemons } />
-      </App>,
-    );
+    renderWithRouter(<App />);
 
     const pokedexTitle = screen.getByRole('heading',
       { name: /Encountered pokémons/i });
 
     expect(pokedexTitle).toBeDefined();
   });
+
+  // Lint pediu para guardar texto em uma const
+  const pokeNameString = 'pokemon-name';
 
   describe('Testa se é exibido o próximo Pokémon quando o botão é clicado', () => {
     it('O botão deve conter o texto Próximo pokémon', () => {
@@ -34,26 +33,47 @@ describe('Testa o componente Pokedex.js', () => {
       expect(buttonNextPokemon).toBeDefined();
     });
 
-    it('(FALSO+)Os próximos Pokémons devem ser mostrados, um a um, ao clicar', () => {
+    it('Os próximos Pokémons devem ser mostrados, um a um, ao clicar', () => {
       renderWithRouter(
         <App>
           <Pokedex isPokemonFavoriteById={ {} } pokemons={ pokemons } />
         </App>,
       );
 
-      //   const buttonNextPokemon = screen.getByRole('button',
-      //     { name: /Próximo pokémon/i });
+      const buttonNextPokemon = screen.getByRole('button',
+        { name: /Próximo pokémon/i });
 
-    //   pokemons.map((pokemon, index) => {
-    //     const pokeName = pokemon[index].name;
-    //     console.log(pokeName);
-    //     // expect(getPokemon).toBeDefined();
-    //     userEvent.click(buttonNextPokemon);
-    //     return 0;
-    //   });
+      const pokemonNames = [];
+      pokemons.map((pokemon) => pokemonNames.push(pokemon.name));
+
+      pokemonNames.forEach((poke) => {
+        const pokeNameId = screen.getByTestId(pokeNameString);
+        expect(pokeNameId).toHaveTextContent(poke);
+        userEvent.click(buttonNextPokemon);
+      });
     });
 
-    it('(FALSO+)Se estiver no último Pokémon da lista, o clique exibe o primeiro', () => {
+    it('Se estiver no último Pokémon da lista, o clique exibe o primeiro', () => {
+      renderWithRouter(
+        <App>
+          <Pokedex isPokemonFavoriteById={ {} } pokemons={ pokemons } />
+        </App>,
+      );
+
+      const buttonNextPokemon = screen.getByRole('button',
+        { name: /Próximo pokémon/i });
+
+      // É preciso clicar 8x em Próximo Pokémon para chegar ao último
+      const maxClicks = 8;
+      for (let index = 0; index < maxClicks; index += 1) {
+        userEvent.click(buttonNextPokemon);
+      }
+      const pokeNameId = screen.getByTestId(pokeNameString);
+      expect(pokeNameId).toHaveTextContent(/Dragonair/i);
+
+      // Novo clique a partir do último pokémon
+      userEvent.click(buttonNextPokemon);
+      expect(pokeNameId).toHaveTextContent(/Pikachu/i);
     });
   });
 
@@ -61,7 +81,7 @@ describe('Testa o componente Pokedex.js', () => {
     it('Testa se é mostrado apenas um Pokémon por vez', () => {
       renderWithRouter(<App />);
 
-      const pokemon = screen.getAllByTestId('pokemon-name');
+      const pokemon = screen.getAllByTestId(pokeNameString);
       expect(pokemon).toHaveLength(1);
     });
   });
@@ -81,7 +101,7 @@ describe('Testa o componente Pokedex.js', () => {
         'Electric', 'Fire', 'Bug', 'Poison', 'Psychic', 'Normal', 'Dragon',
       ];
 
-      allButtons.map((button, index) => {
+      allButtons.forEach((button, index) => {
         expect(button).toHaveTextContent(allTypes[index]);
       });
     });
@@ -94,7 +114,7 @@ describe('Testa o componente Pokedex.js', () => {
       // OBS: Esse teste aprova também o requisito:
       // "O texto do botão deve corresponder ao nome do tipo, ex. Psychic"
 
-      allButtons.map((button) => {
+      allButtons.forEach((button) => {
         userEvent.click(button);
 
         const pokeType = screen.getByTestId('pokemon-type');
@@ -116,42 +136,43 @@ describe('Testa o componente Pokedex.js', () => {
       // após clicar em todos os botões de tipo.
       const allButtons = screen.getAllByTestId(pokeTypeButton);
 
-      allButtons.map((button) => {
+      allButtons.forEach((button) => {
         userEvent.click(button);
         expect(allButton).toBeInTheDocument();
       });
     });
+  });
+});
 
-    describe('Teste se a Pokédex contém um botão para resetar o filtro', () => {
-      beforeEach(() => renderWithRouter(<App />));
+// Lint reclamou, então precisei separar os blocos de describe
+describe('Teste se a Pokédex contém um botão para resetar o filtro', () => {
+  beforeEach(() => renderWithRouter(<App />));
 
-      it('O texto do botão deve ser All', () => {
-        const allButton = screen.getByRole('button',
-          { name: /All/i });
+  it('O texto do botão deve ser All', () => {
+    const allButton = screen.getByRole('button',
+      { name: /All/i });
 
-        expect(allButton).toBeDefined();
-        expect(allButton).toHaveTextContent(/All/i);
-      });
+    expect(allButton).toBeDefined();
+    expect(allButton).toHaveTextContent(/All/i);
+  });
 
-      it('Quando o botão All for clicado, deve mostrar os Pokémons sem filtros', () => {
-        const allButton = screen.getByRole('button',
-          { name: /All/i });
-        const pokeType = screen.getByTestId('pokemon-type');
+  it('Quando o botão All for clicado, deve mostrar os Pokémons sem filtros', () => {
+    const allButton = screen.getByRole('button',
+      { name: /All/i });
+    const pokeType = screen.getByTestId('pokemon-type');
 
-        userEvent.click(allButton);
-        expect(pokeType).toHaveTextContent(/Electric/i);
+    userEvent.click(allButton);
+    expect(pokeType).toHaveTextContent(/Electric/i);
 
-        // Ao clicar em 'Próximo Pokémon', o tipo deve mudar de Electric para Fire.
-        const buttonNextPokemon = screen.getByRole('button',
-          { name: /Próximo pokémon/i });
-        userEvent.click(buttonNextPokemon);
+    // Ao clicar em 'Próximo Pokémon', o tipo deve mudar de Electric para Fire.
+    const buttonNextPokemon = screen.getByRole('button',
+      { name: /Próximo pokémon/i });
+    userEvent.click(buttonNextPokemon);
 
-        expect(pokeType).toHaveTextContent(/Fire/i);
-      });
+    expect(pokeType).toHaveTextContent(/Fire/i);
+  });
 
-      it('Ao carregar a página, o filtro selecionado deverá ser All', () => {
+  it('Ao carregar a página, o filtro selecionado deverá ser All', () => {
 
-      });
-    });
   });
 });
